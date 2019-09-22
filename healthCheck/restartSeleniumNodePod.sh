da5b3bbdcd8e06706adef2bd540c157506393536
srikant noorani <srikant.noorani@broadcom.com>
2019-09-22 15:49:51 +0000

    fix selenium

--- healthCheck/restartSeleniumNodePod.sh
+++ healthCheck/restartSeleniumNodePod.sh
@@ -1,4 +1,4 @@
-SELENIUM_NODE_PODS=`get pod -n selenium|grep node-chrome|grep -v NAME |grep -v grep |awk '{print $1}`
+SELENIUM_NODE_PODS=`kubectl get pods -n selenium|grep node-chrome|grep -v NAME |grep -v grep |awk '{print $1}'`
 
 for NODE_POD in $SELENIUM_NODE_PODS; 
 do
