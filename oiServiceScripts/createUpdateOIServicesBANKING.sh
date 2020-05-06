6e0c53c3b123919daff3384adbc9281bf8cef1c6
ns657061 <srikant.noorani@broadcom.com>
2020-05-06 23:38:57 +0000

    OI related ASM changes

--- oiServiceScripts/createUpdateOIServicesBANKING.sh
+++ oiServiceScripts/createUpdateOIServicesBANKING.sh
@@ -458,6 +458,12 @@ curl -X POST \
                                 "attributeName": "hostname",
                 "attributeValue": "EMEA_DB_HOST_POD_NAME"
         }]
+     },{
+        "metrics": [{
+                 "type": "availability",
+                "sourceName": "ASM_AGENT_NAME",
+                "attributeValue": "ASM_METRIC_NAME"
+        }]
     }]
 }'
 
