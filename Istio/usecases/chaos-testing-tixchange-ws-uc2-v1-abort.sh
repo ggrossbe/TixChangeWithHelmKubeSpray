7eae0c2621694f3aa3a3a2c71a12201a9939e2b4
ns657061 <srikant.noorani@broadcom.com>
2021-01-26 00:34:24 +0000

    istio file changes

--- Istio/usecases/chaos-testing-tixchange-ws-uc2-v1-abort.sh
+++ Istio/usecases/chaos-testing-tixchange-ws-uc2-v1-abort.sh
@@ -1,3 +1,5 @@
-echo "Chaos Testing - Introducing 500 error in v1 version of svc layer"
-kubectl apply -f tixchange-ws-uc2-dest-rule-v1-v2-common.yml  -f chaos-testing-tixchange-ws-uc2-v1-abort.yml -n tixchange-v2
+echo "****Chaos Testing - Introducing 500 error in v1 version of svc layer `date`"
+echo ""
+kubectl apply -f uc2/tixchange-ws-uc2-dest-rule-v1-v2-common.yml  -f uc2/chaos-testing-tixchange-ws-uc2-v1-abort.yml -n tixchange-v2
+
 
--- Istio/usecases/chaos-testing-tixchange-ws-uc2-v1-delay.sh
+++ Istio/usecases/chaos-testing-tixchange-ws-uc2-v1-delay.sh
@@ -1,3 +1,4 @@
-echo "Chaos Testing - introducing 7s delay in version v1 svc layer calls"
+echo "****Chaos Testing - introducing 7s delay in version v1 svc layer calls - `date`"
+echo ""
 kubectl apply -f uc2/tixchange-ws-uc2-dest-rule-v1-v2-common.yml  -f uc2/chaos-testing-tixchange-ws-uc2-v1-delay.yml -n tixchange-v2
 
