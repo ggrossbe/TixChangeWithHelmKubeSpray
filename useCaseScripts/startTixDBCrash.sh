482ecd7d81e43b68e04e356ba0780f636e7e0684
Srikant Noorani <srikant.noorani@broadcom.com>
2023-03-15 09:07:30 -0400

    Update startTixDBCrash.sh

--- useCaseScripts/startTixDBCrash.sh
+++ useCaseScripts/startTixDBCrash.sh
@@ -2,6 +2,7 @@
 #!/bin/bash
 UC_FOLDER=`dirname $BASH_SOURCE`
 
-kubectl delete deployment tix-mysql-deploy -n tixchange-v2
+#kubectl delete deployment tix-mysql-deploy -n tixchange-v2
+kubectl scale -n tixchange-v2 --replicas=0 deployment tix-mysql-deploy
 
 $UC_FOLDER/openAccessUCStatusTracker/UC2_StatusTracker.sh ON
